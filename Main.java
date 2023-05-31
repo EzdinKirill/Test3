ba038eb7b750d1e0f9455d8e7c586123afb69b29
EzdinKirill <ezdinkirill@mail.ru>
2023-05-31 16:17:06 +0700

    2 provlems

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        // Создаем объект акций с именем SB и обозначением ПАО
+        /** Создаем объект акций с именем SB и обозначением ПАО */
         Stock stock = new Stock("SB", "ПАО", 281.50, 282.87);
 
-        // Выводим процент изменения стоимости акций
-        System.out.printf("Процент изменения стоимости акций %s (%s): %.2f%%", stock.getName, stock.getSymbol, stock.getChangePercent());
+        /** Выводим процент изменения стоимости акций */
+        System.out.println("Procent izmeneniya stoimosti akcii: " + stock.getChangePercent() + "%");
     }
 }
--- Stock.java
+++ Stock.java
@@ -1,21 +1,21 @@
-    public class Stock {
-        private String symbol; // Обозначение акций
-        private String name; // Наименование акций
-        private double previousClosingPrice; // Стоимость акций на момент закрытия предыдущего дня
-        private double currentPrice; // Стоимость акций в настоящий момент
+public class Stock {
+    private String symbol; /** Обозначение акций */
+    private String name; /** Наименование акций */
+    private double previousClosingPrice; /** Стоимость акций на момент закрытия предыдущего дня */
+    private double currentPrice; /** Стоимость акций в настоящий момент */
         
-        // Конструктор класса Stock
-        public Stock(String symbol, String name, double previousClosingPrice, double currentPrice) {
-            // Инициализация полей данных
-            this.symbol = symbol;
-            this.name = name;
-            this.previousClosingPrice = previousClosingPrice;
-            this.currentPrice = currentPrice;
-        }
-        
-        // Метод для получения процентного изменения стоимости акций
-        public double getChangePercent() {
-            double percentChange = ((this.currentPrice - this.previousClosingPrice) / this.previousClosingPrice) * 100;
-            return Double.parseDouble(String.format("%.2f", percentChange)); // Возвращаем процентное изменение, округленное до двух десятичных знаков
-        }
+    /** Конструктор класса Stock */
+     public Stock(String symbol, String name, double previousClosingPrice, double currentPrice) {
+        /** Инициализация полей данных */
+        this.symbol = symbol;
+        this.name = name;
+        this.previousClosingPrice = previousClosingPrice;
+        this.currentPrice = currentPrice;
     }
+        
+    /** Метод для получения процентного изменения стоимости акций */
+    public double getChangePercent() {
+        double percentChange = ((this.currentPrice - this.previousClosingPrice) / this.previousClosingPrice) * 100;
+        return Double.parseDouble(String.format("%.2f", percentChange)); /** Возвращаем процентное изменение, округленное до двух десятичных знаков */
+    }
+}
