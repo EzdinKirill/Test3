47ae09dd73a7fd4ae7c535dd96fd02e59a1ce2bf
EzdinKirill <ezdinkirill@mail.ru>
2023-05-31 11:02:09 +0700

    5 errors

--- Stock.java
+++ Stock.java
@@ -1,22 +1,31 @@
-class Stock {
-    // Конструктор класса Stock
-    constructor(symbol, name, previousClosingPrice, currentPrice) {
-        // Инициализация полей данных
-        this.symbol = symbol; // Обозначение акций
-        this.name = name; // Наименование акций
-        this.previousClosingPrice = previousClosingPrice; // Стоимость акций на момент закрытия предыдущего дня
-        this.currentPrice = currentPrice; // Стоимость акций в настоящий момент
+    public class Stock {
+        private String symbol; // Обозначение акций
+        private String name; // Наименование акций
+        private double previousClosingPrice; // Стоимость акций на момент закрытия предыдущего дня
+        private double currentPrice; // Стоимость акций в настоящий момент
+        
+        // Конструктор класса Stock
+        public Stock(String symbol, String name, double previousClosingPrice, double currentPrice) {
+            // Инициализация полей данных
+            this.symbol = symbol;
+            this.name = name;
+            this.previousClosingPrice = previousClosingPrice;
+            this.currentPrice = currentPrice;
+        }
+        
+        // Метод для получения процентного изменения стоимости акций
+        public double getChangePercent() {
+            double percentChange = ((this.currentPrice - this.previousClosingPrice) / this.previousClosingPrice) * 100;
+            return Double.parseDouble(String.format("%.2f", percentChange)); // Возвращаем процентное изменение, округленное до двух десятичных знаков
+        }
     }
     
-    // Метод для получения процентного изменения стоимости акций
-    getChangePercent() {
-        let percentChange = ((this.currentPrice - this.previousClosingPrice) / this.previousClosingPrice) * 100;
-        return percentChange.toFixed(2); // Возвращаем процентное изменение, округленное до двух десятичных знаков
+    public class Main {
+        public static void main(String[] args) {
+            // Создаем объект акций с именем SB и обозначением ПАО
+            Stock stock = new Stock("SB", "ПАО", 281.50, 282.87);
+    
+            // Выводим процент изменения стоимости акций
+            System.out.printf("Процент изменения стоимости акций %s (%s): %.2f%%", stock.getName(), stock.getSymbol(), stock.getChangePercent());
+        }
     }
-}
-
-// Создаем объект акций с именем SB и обозначением ПАО
-let stock = new Stock('SB', 'ПАО', 281.50, 282.87);
-
-// Выводим процент изменения стоимости акций
-console.log(`Процент изменения стоимости акций ${stock.name} (${stock.symbol}): ${stock.getChangePercent()}%`);
