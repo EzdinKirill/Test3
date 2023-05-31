9976c1dfaf8d1e81ccccc26cf35a6dee3be5aa3c
EzdinKirill <ezdinkirill@mail.ru>
2023-05-31 11:12:43 +0700

    2 errors

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Создаем объект акций с именем SB и обозначением ПАО
+        Stock stock = new Stock("SB", "ПАО", 281.50, 282.87);
+
+        // Выводим процент изменения стоимости акций
+        System.out.printf("Процент изменения стоимости акций %s (%s): %.2f%%", stock.getName(), stock.getSymbol(), stock.getChangePercent());
+    }
+}
--- Stock.java
+++ Stock.java
@@ -19,13 +19,3 @@
             return Double.parseDouble(String.format("%.2f", percentChange)); // Возвращаем процентное изменение, округленное до двух десятичных знаков
         }
     }
-    
-    public class Main {
-        public static void main(String[] args) {
-            // Создаем объект акций с именем SB и обозначением ПАО
-            Stock stock = new Stock("SB", "ПАО", 281.50, 282.87);
-    
-            // Выводим процент изменения стоимости акций
-            System.out.printf("Процент изменения стоимости акций %s (%s): %.2f%%", stock.getName(), stock.getSymbol(), stock.getChangePercent());
-        }
-    }
