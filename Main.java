e7424f5d292757c5408342322aca11f99bdd44f9
EzdinKirill <ezdinkirill@mail.ru>
2023-06-02 10:41:50 +0700

    Рабочая программа

--- Main.java
+++ Main.java
@@ -1,9 +1,67 @@
+import java.util.Scanner;
+
 public class Main {
+    private Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
         /** Создаем объект акций с именем SB и обозначением ПАО */
         Stock stock = new Stock("SB", "ПАО", 281.50, 282.87);
-
+        
+        System.out.println("\n********************************************");
+        System.out.println("\nПрограмма для представления акции компании");
+        System.out.println("\n********************************************");
+      
+        Main choiceMenu = new Main();
+    
+        int choice = choiceMenu.getMenuSelection();
+    
+        if(choice == 1){
+    
+        /** Выводим данные об акции */
+        System.out.println("Данные об акции: " + "\nНаименование акции: " + stock.symbol + "\nОбозначение акции: " + stock.name + "\nСтоимость акции на момент закрытия предыдущего дня: " + stock.previousClosingPrice + "\nСтоимость акции в настоящий момент: " + stock.currentPrice);
+        
+        }
+    
+        else if(choice == 2){
+     
         /** Выводим процент изменения стоимости акций */
-        System.out.println("Procent izmeneniya stoimosti akcii: " + stock.getChangePercent() + "%");
+        System.out.println("Процент изменения стоимости акции: " + stock.getChangePercent() + "%");
+        
+        }
+    
+        else {
+      
+        }
+    
     }
+    
+        public int getMenuSelection(){
+            int choice;
+        
+            Scanner input = new Scanner(System.in);
+          
+            while(true){
+        
+            System.out.println("\tОсновное меню:");
+            System.out.println("1: Отображение данных об акции:");
+            System.out.println("2: Отображение процента изменения стоимости акции:");
+            System.out.print("\tВвод:");
+        
+            choice = input.nextInt();
+            System.out.println("\n********************************************");
+      
+            if(choice < 1 || choice > 2){
+            System.out.println("\tНекорректный пункт меню:");
+        
+            System.out.println("\n--------------------------------------------");
+      
+              }
+              
+            else {
+              break;
+              }
+            }
+        
+            return choice;
+      
+        }
 }
--- Stock.java
+++ Stock.java
@@ -1,8 +1,8 @@
 public class Stock {
-    private String symbol; /** Обозначение акций */
-    private String name; /** Наименование акций */
-    private double previousClosingPrice; /** Стоимость акций на момент закрытия предыдущего дня */
-    private double currentPrice; /** Стоимость акций в настоящий момент */
+    public String symbol; /** Обозначение акции */
+    public String name; /** Наименование акции */
+    public double previousClosingPrice; /** Стоимость акции на момент закрытия предыдущего дня */
+    public double currentPrice; /** Стоимость акции в настоящий момент */
         
     /** Конструктор класса Stock */
      public Stock(String symbol, String name, double previousClosingPrice, double currentPrice) {
@@ -13,7 +13,7 @@ public class Stock {
         this.currentPrice = currentPrice;
     }
         
-    /** Метод для получения процентного изменения стоимости акций */
+    /** Метод для получения процентного изменения стоимости акции */
     public double getChangePercent() {
         double percentChange = ((this.currentPrice - this.previousClosingPrice) / this.previousClosingPrice) * 100;
         return Double.parseDouble(String.format("%.2f", percentChange)); /** Возвращаем процентное изменение, округленное до двух десятичных знаков */
