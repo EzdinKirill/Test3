e4fe255b78c98b25d9c79e5c37837f4d1c956365
EzdinKirill <ezdinkirill@mail.ru>
2023-06-06 13:14:26 +0700

    с комментариями

--- Main.java
+++ Main.java
@@ -1,67 +1,70 @@
 import java.util.Scanner;
 
 public class Main {
-    private Scanner input = new Scanner(System.in);
-    public static void main(String[] args) {
-        /** Создаем объект акций с именем SB и обозначением ПАО */
-        Stock stock = new Stock("SB", "ПАО", 281.50, 282.87);
+  private Scanner input = new Scanner(System.in);
+  public static void main(String[] args) {
+    /** Создаем объект акции с именем SB, обозначением ПАО, стоимости на момент закрытия предыдущего дня = 281.50, стоимости акции в настоящий момент = 282.87 */
+    Stock stock = new Stock("SB", "ПАО", 281.50, 282.87);
         
-        System.out.println("\n********************************************");
-        System.out.println("\nПрограмма для представления акции компании");
-        System.out.println("\n********************************************");
+    System.out.println("\n********************************************");
+    System.out.println("\nПрограмма для представления акции компании");
+    System.out.println("\n********************************************");
       
-        Main choiceMenu = new Main();
+    Main choiceMenu = new Main();
     
-        int choice = choiceMenu.getMenuSelection();
+    int choice = choiceMenu.getMenuSelection();
     
-        if(choice == 1){
+    if(choice == 1){
     
-        /** Выводим данные об акции */
-        System.out.println("Данные об акции: " + "\nНаименование акции: " + stock.symbol + "\nОбозначение акции: " + stock.name + "\nСтоимость акции на момент закрытия предыдущего дня: " + stock.previousClosingPrice + "\nСтоимость акции в настоящий момент: " + stock.currentPrice);
+    /** Выводим данные об акции */
+    System.out.println("Данные об акции: " + "\nНаименование акции: " + stock.symbol + "\nОбозначение акции: " + stock.name + "\nСтоимость акции на момент закрытия предыдущего дня: " + stock.previousClosingPrice + "\nСтоимость акции в настоящий момент: " + stock.currentPrice);
         
-        }
-    
-        else if(choice == 2){
-     
-        /** Выводим процент изменения стоимости акций */
-        System.out.println("Процент изменения стоимости акции: " + stock.getChangePercent() + "%");
-        
-        }
-    
-        else {
-      
-        }
-    
     }
     
-        public int getMenuSelection(){
-            int choice;
+    else if(choice == 2){
+     
+    /** Выводим процент изменения стоимости акции */
+    System.out.println("Процент изменения стоимости акции: " + stock.getChangePercent() + "%");
         
-            Scanner input = new Scanner(System.in);
+    }
+    
+    else {
+      
+    }
+    
+  }
+  
+  /** Метод для получения меню выбора программы */  
+  public int getMenuSelection(){
+    int choice;
+        
+    Scanner input = new Scanner(System.in);
           
-            while(true){
+    while(true){
         
-            System.out.println("\tОсновное меню:");
-            System.out.println("1: Отображение данных об акции:");
-            System.out.println("2: Отображение процента изменения стоимости акции:");
-            System.out.print("\tВвод:");
+    System.out.println("\tОсновное меню:");
+    System.out.println("1: Отображение данных об акции:");
+    System.out.println("2: Отображение процента изменения стоимости акции:");
+    System.out.print("\tВвод:");
         
-            choice = input.nextInt();
-            System.out.println("\n********************************************");
+    choice = input.nextInt();
+    System.out.println("\n********************************************");
       
-            if(choice < 1 || choice > 2){
-            System.out.println("\tНекорректный пункт меню:");
+    if(choice < 1 || choice > 2){
+    System.out.println("\tНекорректный пункт меню:");
         
-            System.out.println("\n--------------------------------------------");
+    System.out.println("\n--------------------------------------------");
       
-              }
+    }
               
-            else {
-              break;
-              }
-            }
-        
-            return choice;
+    else {
+      break;
+    }
       
-        }
+    }
+        
+    return choice;
+      
+  }
+  
 }
--- Stock.java
+++ Stock.java
@@ -3,19 +3,20 @@ public class Stock {
     public String name; /** Наименование акции */
     public double previousClosingPrice; /** Стоимость акции на момент закрытия предыдущего дня */
     public double currentPrice; /** Стоимость акции в настоящий момент */
-        
+          
     /** Конструктор класса Stock */
-     public Stock(String symbol, String name, double previousClosingPrice, double currentPrice) {
-        /** Инициализация полей данных */
-        this.symbol = symbol;
-        this.name = name;
-        this.previousClosingPrice = previousClosingPrice;
-        this.currentPrice = currentPrice;
+    public Stock(String symbol, String name, double previousClosingPrice, double currentPrice) {
+      /** Инициализация полей данных */
+      this.symbol = symbol;
+      this.name = name;
+      this.previousClosingPrice = previousClosingPrice;
+      this.currentPrice = currentPrice;
     }
-        
+          
     /** Метод для получения процентного изменения стоимости акции */
     public double getChangePercent() {
-        double percentChange = ((this.currentPrice - this.previousClosingPrice) / this.previousClosingPrice) * 100;
-        return Double.parseDouble(String.format("%.2f", percentChange)); /** Возвращаем процентное изменение, округленное до двух десятичных знаков */
+      double percentChange = ((this.currentPrice - this.previousClosingPrice) / this.previousClosingPrice) * 100; /** Находим процентное изменение, используя для расчётов значения стоимости акции на момент закрытия предыдущего дня и стоимости акции в настоящий момент */
+      return Double.parseDouble(String.format("%.2f", percentChange)); /** Возвращаем процентное изменение, округленное до двух десятичных знаков */
     }
-}
+    
+  }
